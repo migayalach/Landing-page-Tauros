f5a6773c3ccd83c928b3eb25c39c41a49001a511
migayalach <ayalachavezmiguel@gmail.com>
2025-06-11 12:05:37 -0400

    export new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,8 +2,10 @@ export * from "./button";
 export * from "./card";
 export * from "./form";
 export * from "./navbar";
-export * from "./modal"
-export * from "./message"
-export * from "./alert"
+export * from "./modal";
+export * from "./message";
+export * from "./alert";
+export * from "./userInfo";
+export * from "./results";
 
 export { default as Carousell } from "./carousel/Carousell";
