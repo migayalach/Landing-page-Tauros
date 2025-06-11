50a675069bb6adbac9445776c7ffe26e63b84bfc
migayalach <ayalachavezmiguel@gmail.com>
2025-06-10 23:06:38 -0400

    added metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { icons } from "antd/es/image/PreviewGroup";
+
+export const metadata = {
+  title: "Tauros",
+  description:
+    "Compra gelatinas de pata naturales en Tauros. Fortalece tus articulaciones con productos saludables y de calidad. Envíos rápidos y atención personalizada.",
+};
+
 export default function Home() {
   return (
     <main>
