fabb443400e8e0c50d9b6237fe7dec36bf001edb
migayalach <ayalachavezmiguel@gmail.com>
2025-06-11 00:07:22 -0400

    created components modal

---
+++ src/components/modal/index.ts
@@ -0,0 +1,1 @@
+export { default as ModalOrder } from "./modalOrder/ModalOrder";
---
+++ src/components/modal/modalOrder/ModalOrder.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ModalOrder() {
+  return (
+    <div>ModalOrder</div>
+  )
+}
+
+export default ModalOrder
