8b1b8051d05dd7c1780592e4df55eb3c3ed8acfa
migayalach <ayalachavezmiguel@gmail.com>
2025-06-11 00:07:02 -0400

    created components message

---
+++ src/components/message/index.ts
@@ -0,0 +1,3 @@
+export { default as MessageError } from "./messageError/MessageError";
+export { default as MessageInfo } from "./messageInfo/MessageInfo";
+export { default as MessageSuccess } from "./messageSuccess/MessageSuccess";
---
+++ src/components/message/messageSuccess/MessageSuccess.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MessageSuccess() {
+  return (
+    <div>MessageSuccess</div>
+  )
+}
+
+export default MessageSuccess
