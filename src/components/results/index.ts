ebda93782a7de34945ce56f021523f36ac0943fa
migayalach <ayalachavezmiguel@gmail.com>
2025-06-11 12:04:22 -0400

    created pages to response to server

---
+++ src/components/results/index.ts
@@ -0,0 +1,4 @@
+export { default as Page403 } from "./page403/Page403";
+export { default as Page404 } from "./page404/Page404";
+export { default as Page500 } from "./page500/Page500";
+export { default as PageLoading } from "./pageLoading/PageLoading";
