d439eca45282e3ce878d7bc1b760ea1ec3028fb0
migayalach <ayalachavezmiguel@gmail.com>
2025-06-11 23:25:10 -0400

    import new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,5 +7,7 @@ export * from "./message";
 export * from "./alert";
 export * from "./userInfo";
 export * from "./results";
+export * from "./images";
+export * from "./carousel";
 
-export { default as Carousell } from "./carousel/Carousell";
+export { default as Footer } from "./footer/Footer";
