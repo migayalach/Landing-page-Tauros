78d73188dbfbe81c65c470e2781868cbb7ebd0d4
migayalach <ayalachavezmiguel@gmail.com>
2025-06-11 23:22:51 -0400

    render card

--- src/components/card/aCard/Acard.tsx
+++ src/components/card/aCard/Acard.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+// COMPONENTS
+import Description from "../description/Description";
+import { ImageProduct } from "@/components/images";
+// HOOKS
+import React from "react";
+import Link from "next/link";
+// REDUX
+// INTERFACE
+import { InputProduct } from "@/interface/InputAcard.interface";
+// LIBRARY
+import { Card, Col, Row } from "antd";
+// CSS
+// JAVASCRIP
 
-function Acard() {
+function Acard({ infoProduct }: { infoProduct: InputProduct }) {
   return (
-    <div>Acard</div>
-  )
+    <Row gutter={16}>
+      <Col span={8}>
+        <Card title={infoProduct.productName}>
+          <ImageProduct listImages={infoProduct.images} />
+        </Card>
+        <Link href={`/${infoProduct.idProduct}`}>
+          <p>Precio: Bs.- {infoProduct.price}</p>
+          <h1>{infoProduct.productName}</h1>
+          <Description text={infoProduct.description} />
+        </Link>
+      </Col>
+    </Row>
+  );
 }
 
-export default Acard
+export default Acard;
