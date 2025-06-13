0c887e3a3bf61d057479ea652222718b49feaa27
migayalach <ayalachavezmiguel@gmail.com>
2025-06-12 20:04:07 -0400

    created component to tools

--- src/components/form/formContacts/FormContacts.tsx
+++ src/components/form/formContacts/FormContacts.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
+"use client";
+// COMPONENTS
+// HOOKS
+// REDUX
+import React, { useState } from "react";
+// INTERFACE
+// LIBRARY
+import { Button, Form, Input } from "antd";
+// CSS
+// JAVASCRIP
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+
+const validateMessages = {
+  required: "${label} is required!",
+  types: {
+    email: "${label} is not a valid email!",
+    number: "${label} is not a valid number!",
+  },
+  number: {
+    range: "${label} must be between ${min} and ${max}",
+  },
+};
+
+const onFinish = (values: any) => {
+  console.log(values);
+};
 
 function FormContacts() {
+  const [infoUser, setInfoUser] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+
   return (
-    <div>FormContacts</div>
-  )
+    <Form
+      {...layout}
+      name="nest-messages"
+      onFinish={onFinish}
+      style={{ maxWidth: 600 }}
+      validateMessages={validateMessages}
+    >
+      <Form.Item
+        name={["user", "email"]}
+        label="Email"
+        rules={[{ type: "email" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name={["user", "email"]}
+        label="Email"
+        rules={[{ type: "email" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item label={null}>
+        <Button type="primary" htmlType="submit">
+          Enviar
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
 
-export default FormContacts
+export default FormContacts;
