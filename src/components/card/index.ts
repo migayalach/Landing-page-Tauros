0af8caa8236d6901d74cd52a3a3e12ed209b42bd
migayalach <ayalachavezmiguel@gmail.com>
2025-06-13 23:57:11 -0400

    render components

--- src/components/card/index.ts
+++ src/components/card/index.ts
@@ -2,4 +2,3 @@ export { default as Acard } from "./aCard/Acard";
 export { default as Cards } from "./cards/Cards";
 export { default as Description } from "./description/Description";
 export { default as Feadback } from "./feedback/Feedback";
-export { default as Card } from "./Card";
