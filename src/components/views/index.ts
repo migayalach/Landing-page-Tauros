d8e2fe635f14e5f4ce2388b29ace9ee59289098e
migayalach <ayalachavezmiguel@gmail.com>
2025-06-13 23:53:35 -0400

    export components

--- src/components/views/index.ts
+++ src/components/views/index.ts
@@ -1,3 +1,6 @@
+import { default as Products } from "./products/Products";
+import { default as Owners } from "./owners/Owners";
+import { default as Footer } from "./footer/Footer";
 import { default as Calls } from "./calls/Calls";
 
-export { Calls };
+export { Products, Owners, Footer, Calls };
