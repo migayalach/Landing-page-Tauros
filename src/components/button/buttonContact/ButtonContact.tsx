5fd6dfb8a3ac63a273f79da95c60df423fa64d6d
migayalach <ayalachavezmiguel@gmail.com>
2025-06-14 00:02:45 -0400

    delete files

--- src/components/button/buttonContact/ButtonContact.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ButtonContact() {
-  return (
-    <div>ButtonContact</div>
-  )
-}
-
-export default ButtonContact
--- src/components/button/buttonSend/ButtonSend.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ButtonSend() {
-  return (
-    <div>ButtonSend</div>
-  )
-}
-
-export default ButtonSend
