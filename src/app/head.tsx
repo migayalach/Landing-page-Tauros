7b0cacfe963fbb2e67116a387bda97d2936393c5
migayalach <ayalachavezmiguel@gmail.com>
2025-06-13 23:46:02 -0400

    created head to google fonts

---
+++ src/app/head.tsx
@@ -0,0 +1,17 @@
+export default function Head() {
+  return (
+    <>
+      {/* TODO Google Fonts */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,400;0,700&family=Caprasimo&family=Paytone+One&display=swap"
+        rel="stylesheet"
+      />
+    </>
+  );
+}
