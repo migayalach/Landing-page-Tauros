ade4f70e0612e048888b7eec9eb49b8f603abda8
migayalach <ayalachavezmiguel@gmail.com>
2025-06-13 23:51:50 -0400

    creted component footer

---
+++ src/components/views/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Calls from "../calls/Calls";
+
+function Footer() {
+  return (
+    <div className="flex flex-col ">
+      <Calls />
+      <div>
+        <h1>Nuestas redes</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
