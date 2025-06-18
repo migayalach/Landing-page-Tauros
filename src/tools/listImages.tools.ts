4087e950cef9c5c34ad3956d93220f5209c9cc34
migayalach <ayalachavezmiguel@gmail.com>
2025-06-17 23:05:26 -0400

    update pictures

--- src/tools/listImages.tools.ts
+++ src/tools/listImages.tools.ts
@@ -1,4 +1,6 @@
 export const listImages = [
-  "https://www.suzuki.com.bo/media/j0fnbkow/01_suzuki_landing_1927x650.png",
+  "https://www.suzuki.com.bo/media/2izbhox3/portadas-homebanner-all-new-scross-1927x650.png",
+  "https://www.suzuki.com.bo/media/2izbhox3/portadas-homebanner-all-new-scross-1927x650.png",
+  "https://www.suzuki.com.bo/media/2izbhox3/portadas-homebanner-all-new-scross-1927x650.png",
   "https://www.suzuki.com.bo/media/2izbhox3/portadas-homebanner-all-new-scross-1927x650.png",
 ];
