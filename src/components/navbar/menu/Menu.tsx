5e93d052d38a67e7ae4ec02c6594bad35b8ffd53
migayalach <ayalachavezmiguel@gmail.com>
2025-06-17 23:12:37 -0400

    render component

--- src/components/navbar/menu/Menu.tsx
+++ src/components/navbar/menu/Menu.tsx
@@ -1,19 +1,54 @@
+"use client";
 // COMPONENTS
 // HOOKS
-import React from 'react'
+import React from "react";
 // REDUX
 // INTERFACE
 // LIBRARY
+import { Layout, Menu } from "antd";
 // CSS
-import "./menu.css"
+import "./menu.css";
+import Link from "next/link";
 // JAVASCRIP
 
-function Menu() {
+const { Header } = Layout;
+
+const items = [
+  { key: "1", label: <a href="#products">Productos</a> },
+  { key: "2", label: <a href="#us">Quienes somos</a> },
+  { key: "3", label: <a href="#foorter">Contactanos</a> },
+];
+
+function NavBar() {
   return (
-    <div 
-    // className="container-menu"
-    >Menu</div>
-  )
+    <Layout>
+      <Header
+        className="fixed-header"
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div className="logo">
+          <Link href="./">
+            <img src="https://res.cloudinary.com/dqgcyonb9/image/upload/v1750174385/Taurus/alhq3q0kvwtozm2suru4.png" />
+          </Link>
+        </div>
+
+        <div className="name-company">
+          <h1>Taurus</h1>
+        </div>
+
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          // defaultSelectedKeys={["1"]}
+          items={items}
+          className="menu-info"
+        />
+      </Header>
+    </Layout>
+  );
 }
 
-export default Menu
+export default NavBar;
