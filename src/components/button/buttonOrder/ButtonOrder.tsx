359c14359e9b6c737d12387aebc6754f7193b12e
migayalach <ayalachavezmiguel@gmail.com>
2025-06-18 14:11:10 -0400

    update button and added new event to name handleSendMessage

--- src/components/button/buttonOrder/ButtonOrder.tsx
+++ src/components/button/buttonOrder/ButtonOrder.tsx
@@ -1,23 +1,31 @@
+"use client";
 // COMPONENTS
 // HOOKS
 import React from "react";
 // REDUX
 // INTERFACE
 // LIBRARY
-import { FloatButton } from "antd";
+import { WhatsAppOutlined } from "@ant-design/icons";
 // CSS
 // JAVASCRIP
 
 function ButtonOrder() {
+  const handleSendMessage = () => {
+    const phone = `${process.env.NEXT_PUBLIC_PHONE}`;
+    const message =
+      "Hola, quiero más información de como puedo adquirir productos.";
+    const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+    window.open(url, "_blank");
+  };
+
   return (
-    <FloatButton
-      style={{ marginRight: "15px" }}
-      tooltip={{
-        title: "¡Contactanos!",
-        color: "#8ac926",
-        placement: "top",
-      }}
-    />
+    <button
+      onClick={handleSendMessage}
+      className="fixed bottom-6 right-6 bg-[#25d366] text-white p-3 rounded-full shadow-lg hover:scale-110 transition-all z-50"
+      aria-label="Contáctanos por WhatsApp"
+    >
+      <WhatsAppOutlined style={{ fontSize: "24px" }} />
+    </button>
   );
 }
 
