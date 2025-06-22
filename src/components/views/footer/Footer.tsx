a6eccc7b8638882f58d539edf340ec5f290bbc3b
migayalach <ayalachavezmiguel@gmail.com>
2025-06-22 19:20:57 -0400

    update styles

--- src/components/views/footer/Footer.tsx
+++ src/components/views/footer/Footer.tsx
@@ -18,11 +18,11 @@ function Footer() {
       <div>
         <hr className="bg-black h-[1px] w-full" />
         <div className="flex flex-row justify-center items-center pt-3 ">
-          <h1>¡Síguenos en las redes sociales!</h1>
-          <div className="pl-12">
+          <h1 className="text-[#1b1b32]">¡Síguenos en las redes! <span className="text-[20px]">👉🏻</span></h1>
+          <div className="flex flex-row justify-around gap-5 pl-12">
             <ButtonOurNetworks title="facebook" icon="facebookIcon" />
-            <ButtonOurNetworks title="instagram" icon="instagramIcon" />
             <ButtonOurNetworks title="tiktok" icon="tiktokIcon" />
+            <ButtonOurNetworks title="instagram" icon="instagramIcon" />
           </div>
         </div>
       </div>
