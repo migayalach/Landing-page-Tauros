c2d5a8aaed7a8b5320571635b121ed87a80b53ee
migayalach <ayalachavezmiguel@gmail.com>
2025-06-22 22:50:13 -0400

    delete

--- docs/constants.ts
+++
@@ -1,7 +0,0 @@
-export const URL_SERVER_DEPLOY = process.env.NEXT_PUBLIC_SERVER_DEPLOY;
-export const URL_SERVER_LOCAL = process.env.NEXT_PUBLIC_SERVER_LOCAL;
-
-export const URL_SERVER =
-  process.env.NEXT_PUBLIC_DEV === "production"
-    ? URL_SERVER_DEPLOY
-    : URL_SERVER_LOCAL;
