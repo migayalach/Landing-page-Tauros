49f2cf7448ca2e9a5c39bad31610554fadea0eba
migayalach <ayalachavezmiguel@gmail.com>
2025-06-23 23:39:03 -0400

    fase 1 ready

--- src/components/filter/filterContact/FilterContact.tsx
+++ src/components/filter/filterContact/FilterContact.tsx
@@ -1,35 +1,105 @@
 "use client";
-import React from "react";
+// COMPONENTS
+// HOOKS
+import React, { useState } from "react";
+// REDUX
+// INTERFACE
+import { FilterData } from "./filter-interface";
+import type { CheckboxProps } from "antd";
+// LIBRARY
 import { Button, Checkbox, Input, Select } from "antd";
+// CSS
+import "@/styles/text.styles.css";
+// JAVASCRIP
 
 function FilterContact() {
-  const handleChange = (value: string) => {
+  const [flag, setFlag] = useState(false);
+  const [searchPhone, setSearchPhone] = useState<string>("");
+  const [search, setSearch] = useState<FilterData>({
+    value: "",
+    order: "",
+  });
+
+  const handleSearchSelect = (value: string) => {
+    console.log(value);
+  };
+
+  const handleOrder: CheckboxProps["onChange"] = (event) => {
+    // console.log(`checked = ${event.target.checked}`);
+    // console.log(`checked = ${event.target.value}`);
+    // if (event.target.checked) {
+    //   setSearch({
+    //     ...search,
+    //     order: event.target.value,
+    //   });
+    // } else {
+    //   setSearch({
+    //     ...search,
+    //     order: "",
+    //   });
+    // }
+  };
+
+  const handleSearchPhone = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchPhone(event.target.value);
+  };
+
+  const handleSearch = (value: string) => {
     console.log(`selected ${value}`);
   };
 
+  const handleClear = () => {};
+
+  console.log(search);
+
   return (
-    <div>
-      <div>
-        nombre - asc y desc fecha - asc y desc
+    <div className="bg-[#f8f9fa] p-5 ml-2 mr-2 mb-7 rounded-md">
+      <h1 className="title-search mb-2 text-[20px] font-medium ">
+        Buscar personas :
+      </h1>
+      <div className="flex justify-between items-center mb-2 w-full">
+        <p className="title-search"> Nombre o Fecha: </p>
         <Select
           defaultValue=""
           style={{ width: 120 }}
-          onChange={handleChange}
+          onChange={handleSearchSelect}
           options={[
             { value: "", label: "" },
-            { value: "lucy", label: "Nombre" },
-            { value: "Yiminghe", label: "Fecha" },
+            { value: "name", label: "Nombre" },
+            { value: "date", label: "Fecha" },
           ]}
         />
-        <Checkbox>Checkbox</Checkbox>
-        <Checkbox>Checkbox</Checkbox>
+        <div className="flex flex-col">
+          <Checkbox
+            className="title-search"
+            name="asc"
+            value={search.order}
+            onChange={handleOrder}
+          >
+            Asc
+          </Checkbox>
+          <Checkbox
+            className="title-search"
+            name="desc"
+            value={search.order}
+            onChange={handleOrder}
+          >
+            Desc
+          </Checkbox>
+        </div>
       </div>
-      <div>
-        Celular: <Input placeholder="Basic usage" />
+      <div className="flex flex-row items-center mb-2">
+        <p className="title-search mr-2">Celular:</p>
+        <Input
+          onChange={handleSearchPhone}
+          placeholder="75712066"
+          // disabled={!flag ? true : false}
+          className="w-28"
+        />
       </div>
-      <div>
-        <Button>Buscar</Button>
-        <Button>Limpiar</Button>
+      <div className="flex flex-row justify-center items-center mt-5">
+        <Button className="title-search">Buscar</Button>
+        <Button className="title-search ml-5">Limpiar</Button>
       </div>
     </div>
   );
