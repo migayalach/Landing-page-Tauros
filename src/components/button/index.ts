2e1c8844e919af10a7479d8ae51ff49df5c2a5ed
migayalach <ayalachavezmiguel@gmail.com>
2025-06-26 16:26:03 -0400

    added new component to export 'buttonSignModal'

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,3 +1,4 @@
 export { default as ButtonOrder } from "./buttonOrder/ButtonOrder";
 export { default as ButtonMoreAbout } from "./buttonMoreAbout/ButtonMoreAbout";
 export { default as ButtonOurNetworks } from "./buttonOurNetworks/ButtonOurNetworks";
+export { default as ButtonSignOut } from "./buttonSignOut/ButtonSignOut";
