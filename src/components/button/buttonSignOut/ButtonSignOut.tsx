7b45d2d16fced6234e14b1a174182a991b7fdd0a
migayalach <ayalachavezmiguel@gmail.com>
2025-06-26 16:21:39 -0400

    creted component to out session

---
+++ src/components/button/buttonSignOut/ButtonSignOut.tsx
@@ -0,0 +1,19 @@
+// COMPONENTS
+// HOOKS
+import React from "react";
+// REDUX
+// INTERFACE
+// LIBRARY
+import { UserDeleteOutlined } from "@ant-design/icons";
+// CSS
+// JAVASCRIP
+
+function ButtonSignOut() {
+  return (
+    <button className="text-[#fff] hover:text-[#f8f9fa]">
+      <UserDeleteOutlined />
+    </button>
+  );
+}
+
+export default ButtonSignOut;
